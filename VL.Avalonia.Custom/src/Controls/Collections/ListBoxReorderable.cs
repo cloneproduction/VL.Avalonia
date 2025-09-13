ee93fe0c9fe6839623357242d5a4a79a5eb94092
cloneproduction <clone@cloneproduction.net>
2025-09-13 20:14:38 +0800

    Starting implementing

---
+++ VL.Avalonia.Custom/src/Controls/Collections/ListBoxReorderable.cs
@@ -0,0 +1,16 @@
+﻿using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
+using Avalonia.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VL.Avalonia.Custom.Controls.Collections
+{
+    public partial class ListBoxReorderable : ListBox
+    {
+        protected override Type StyleKeyOverride { get { return typeof(ListBox); } }
+    }
+}
---
+++ VL.Avalonia.Custom/src/Controls/Collections/ListBoxReorderableWrapper.cs
@@ -0,0 +1,18 @@
+﻿using Avalonia.Controls;
+using VL.Avalonia.Attributes;
+using VL.Avalonia.Controls;
+using VL.Core;
+using VL.Core.Import;
+
+namespace VL.Avalonia.Custom.Controls.Collections
+{
+    /// <summary>
+    /// Listbox with reorderable items
+    /// </summary>
+    [ProcessNode(Name = "ListBoxReorderable")]
+    public partial class ListBoxReorderableWrapper<T> : SelectingItemsControlWrapperBase<ListBoxReorderable, T>
+    {
+        [ImplementProperty("ListBoxReorderable.SelectionModeProperty", PinVisibility = Model.PinVisibility.Optional)]
+        protected Optional<SelectionMode> _selectionMode;
+    }
+}
